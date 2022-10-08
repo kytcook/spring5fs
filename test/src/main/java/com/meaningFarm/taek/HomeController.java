a6b509c76bbccd64357f0c8c515bad4c03bd8b4d
kytcook <kytcook@naver.com>
2022-10-08 14:25:12 +0900

    springMavenStart_setting_221028

---
+++ test/src/main/java/com/meaningFarm/taek/HomeController.java
@@ -0,0 +1,41 @@
+package com.meaningFarm.taek;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		// 현재날짜 객체 생성
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		// model : 자바프로그램에서 화면단과 왔다갔다 할 때 사용. request가 내장되어 있다.
+		model.addAttribute("serverClock", formattedDate );
+		
+		return "home";
+	}
+	
+}
