49f48ece19a936f1d4f49ea3d26b69e58ab6912c
kytcook <kytcook@naver.com>
2022-10-11 15:30:26 +0900

    koreaEncodingFilter_errorfix_221011

--- test/src/main/java/com/meaningFarm/board/controller/BoardController.java
+++ test/src/main/java/com/meaningFarm/board/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.meaningFarm.board.vo.BoardVO;
 import com.webjjang.util.PageObject;
 
 // 자동생성 - @Controller, @Service, @Repository, @Compnent, @RestController, @Advice
@@ -16,68 +17,54 @@ import com.webjjang.util.PageObject;
 public class BoardController {
 
 	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
-	
+
 	// 게시판 리스트
 	@GetMapping("/list.do")
-	public String list(PageObject pageObject) throws Exception{
-		
+	public String list(PageObject pageObject) throws Exception {
 		log.info("게시판 리스트 처리");
-		
 		return "board/list";
 	}
-	
-	// 게시판 글쓰기 폼
-	@GetMapping("/write.do")
-	public String writeForm() throws Exception{
-		
-		log.info("게시판 글쓰기 폼");
-		
-		return "board/write";
-	}
-	
-	// 게시판 글쓰기 처리
-	@PostMapping("/write.do")
-	public String write() throws Exception{
-		
-		log.info("게시판 글쓰기 처리");
-		
-		return "redirect:list.do";
-	}
-	
+
 	// 게시판 글보기
 	@GetMapping("/view.do")
-	public String view(long no) throws Exception{
-		
-		log.info("게시판 글보기 - no: "+ no);
-		
+	public String view(long no, int inc) {
+		log.info("게시판 글보기 - no: " + no + ", inc : " + inc);
 		return "board/view";
 	}
-	
-	// 게시판 수정 폼
-	@GetMapping("/update.do")
-	public String updateForm(long no) throws Exception{
-		
-		log.info("게시판 수정 폼 - no : " + no);
-		
-		return "board/update";
+
+	// 게시판 글쓰기 폼
+	@GetMapping("/write.do")
+	public String writeForm() {
+		log.info("게시판 글쓰기 폼");
+		return "board/write";
 	}
-	
-	// 게시판 수정 처리
-	@PostMapping("/update.do")
-	public String update() throws Exception{
-		
-		log.info("게시판 글수정 처리");
-		
-		return "redirect:view.do?no=10";
-	}
-	
-	// 게시판 글삭제
-	@GetMapping("/delete.do")
-	public String delete(long no) throws Exception{
-		
-		log.info("게시판 글삭제 처리 - no : " + no);
-		
+
+	// 게시판 글쓰기 처리
+	@PostMapping("/write.do")
+	public String write(BoardVO vo) {
+		log.info("게시판 글쓰기 처리 - vo : " + vo);
 		return "redirect:list.do";
 	}
-	
+
+	// 게시판 글수정 폼
+	@GetMapping("/update.do")
+	public String updateForm(long no) {
+		log.info("게시판 수정 폼 - no : " + no);
+		return "board/update";
+	}
+
+	// 게시판 글수정 처리
+	@PostMapping("/update.do")
+	public String update(BoardVO vo) {
+		log.info("게시판 글수정 처리 - vo : " + vo);
+		return "redirect:view.do?no=10";
+	}
+
+	// 게시판 글삭제
+	@GetMapping("/delete.do")
+	public String delete(long no) throws Exception {
+		log.info("게시판 글삭제 처리 - no : " + no);
+		return "redirect:list.do";
+	}
+
 }
